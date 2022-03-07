cc7c039fc5ee6b14bbc4b94a8fa3c39991564355
Don Nijssen <donny.nijssen@gmail.com>
2022-03-07 21:53:15 +0100

    initial commit for memorize project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Don Nijssen on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
