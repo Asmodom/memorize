898a46b372bfd8088c63c9c8139d8ac3acae68e6
Don Nijssen <donny.nijssen@gmail.com>
2022-03-08 23:08:42 +0100

    added on tap to flip cards

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,21 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 25.0)
-                .stroke()
-                .stroke(lineWidth: 3.0)
-
-            Text("Hi donnijssen.com")
-                .foregroundColor(.blue)
+        HStack{
+            CardView(isFaceUp:true)
+            CardView(isFaceUp:false)
+            CardView(isFaceUp:true)
+            CardView(isFaceUp:false)
+            
         }
             .padding(.horizontal)
             .foregroundColor(.red)
     }
 }
 
+
+struct CardView: View{
+    @State var isFaceUp: Bool
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp{
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke()
+                    .stroke(lineWidth: 3.0)
+
+                Text("✈️")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+            }else{
+                shape
+                    .fill()
+            }
+
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //create for each color scheme to see the ui effects
+        ContentView().preferredColorScheme(.light)
+        ContentView().preferredColorScheme(.dark)
     }
 }
